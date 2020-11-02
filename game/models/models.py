842ec883911658c0bfa5af0f165dfe73368fdce4
Carlos <cartegar@gmail.cim>
2020-11-02 10:46:19 +0100

    Regions de tots els members del clan. Kanban dels characters en Player i Region

--- game/models/models.py
+++ game/models/models.py
@@ -10,7 +10,7 @@ class player(models.Model):
     photo = fields.Image(max_width='120')
     name = fields.Char()
     race = fields.Selection([('1', 'Hombre lobo'), ('2', 'Vampiro')])
-    level = fields.Integer()
+    level = fields.Integer(default=1)
     points = fields.Integer()
     description = fields.Text()
     regions = fields.One2many('game.region', 'leader')
@@ -22,20 +22,27 @@ class clan(models.Model):
     _description = 'game.clan'
 
     name = fields.Char()
-    level = fields.Integer()
+    level = fields.Integer(default=1)
     members = fields.One2many('game.player', 'clan')
+    regions = fields.One2many('game.region', 'leader_clan', compute='_get_regions')
+
+    @api.depends('members')
+    def _get_regions(self):
+        for r in self:
+            for m in self.members:
+                r.regions += m.regions
 
 class character(models.Model):
     _name = 'game.character'
     _description = 'game.character'
 
     name = fields.Char()
-    level = fields.Integer()
+    level = fields.Integer(default=1)
     player_leader = fields.Many2one('game.player')
     region = fields.Many2one('game.region')
-    mining_level = fields.Integer();
-    hunting_level = fields.Integer();
-    gathering_level = fields.Integer();
+    mining_level = fields.Integer(default=1);
+    hunting_level = fields.Integer(default=1);
+    gathering_level = fields.Integer(default=1);
 
 
 class region(models.Model):
@@ -43,8 +50,13 @@ class region(models.Model):
     _description = 'game.region'
 
     name = fields.Char()
-    base_level = fields.Integer()
+    fortress_level = fields.Integer()
     max_characters = fields.Integer()
-    leader = fields.Many2one('game.player', ondelete='cascade')
+    leader = fields.Many2one('game.player')
+    leader_clan = fields.Many2one('game.clan', compute='_get_leader_clan')
     characters = fields.One2many('game.character', 'region')
 
+    @api.depends('leader')
+    def _get_leader_clan(self):
+        for r in self:
+            r.leader_clan = r.leader.clan
